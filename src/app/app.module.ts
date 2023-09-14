81adbe7d65ac9d2c2dde050f9016117a8c4dab95
AugustRr <augustredrock@gmail.com>
2023-09-14 23:09:36 +0300

    Initial commit - Skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableComponent } from './table/table.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ButtonComponent } from './button/button.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponent,
+    HeaderComponent,
+    FooterComponent,
+    ButtonComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
